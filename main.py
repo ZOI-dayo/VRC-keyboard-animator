7bee8188ba98fdfd1e5a1a56819c7f4bc1a1d451
zoi <zoi@zoizoi.net>
2022-02-23 23:29:50 +0900

    いちおう動く?送信は確認

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
-import random
-import time
+import json
 
 from pythonosc import udp_client
 from pyhooked import Hook, KeyboardEvent, MouseEvent
@@ -8,18 +7,22 @@ from pyhooked import Hook, KeyboardEvent, MouseEvent
 OSC_IP = "127.0.0.1"
 OSC_PORT = 9000
 
+KEY_LOC = json.load(open('assets/keyLoc.json', 'r'))
+KEY_FINGER = json.load(open('assets/keyFinger.json', 'r'))
+
+
 def main():
-  osc_client = init_osc()
-  osc_client.send_message("/avatar/parameters/VRCSupine", 1)
+  osc_client.send_message("/avatar/parameters/KBDMode", True)
+
 #   send_message(osc_client, "/filter", random.random())
 
 #   for x in range(10):
 #     osc_client.send_message("filter", 1.0)
 #     time.sleep(1)
 #     print("send2")
-#   hook = Hook()
-#   hook.handler = on_keyboard_events
-#   hook.hook()
+  hook = Hook()
+  hook.handler = on_keyboard_events
+  hook.hook()
 
 def init_osc():
   parser = argparse.ArgumentParser()
@@ -32,9 +35,20 @@ def init_osc():
 # def send_message(osc_client, path, value):
 #   osc_client.send_message(path, value)
 
+osc_client = init_osc()
+def send_loc(key):
+  hand = KEY_FINGER[key][0]
+  column = (KEY_LOC[key][1] - 7.5) / 7.5
+  row = (KEY_LOC[key][0] - 2) / 2.0
+  osc_client.send_message("/avatar/parameters/KBDColumn" + hand, column)
+  osc_client.send_message("/avatar/parameters/KBDRow" + hand, row)
+  osc_client.send_message("/avatar/parameters/test01", 1.0)
+  print(hand, column, row)
+
 def on_keyboard_events(args):
   if isinstance(args, KeyboardEvent):
     print(args.key_code, args.current_key, args.event_type)
+    send_loc(args.current_key)
 
   if isinstance(args, MouseEvent):
     print(args.mouse_x, args.mouse_y)
